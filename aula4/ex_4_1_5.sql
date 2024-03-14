79c12097bcb32d8ea0120c31b49d5b06699dd60f
João Monteiro <joaomonteiro29@ua.pt>
2024-03-14 12:05:32 +0000

    ex2

--- aula4/ex_4_1_5.sql
+++ aula4/ex_4_1_5.sql
@@ -2,10 +2,37 @@ CREATE DATABASE Rentacar;
 GO
 USE Rentacar;
 GO
+CREATE TABLE Instituição (
+    nome                VARCHAR NOT NULL,
+    [address]           VARCHAR,
+
+    PRIMARY KEY(nome),
+    UNIQUE([address])
+);
+
 CREATE TABLE Participantes (
     nome                VARCHAR NOT NULL,
     [address]           VARCHAR,
     end_email           VARCHAR,
     data_insc           VARCHAR NOT NULL,
     nome_inst           VARCHAR
+
+    PRIMARY KEY(nome),
+    FOREIGN KEY(nome_inst) REFERENCES Instituição(nome)
+);
+
+CREATE TABLE Não_Estudante (
+    nome                VARCHAR NOT NULL,
+    ref_bancária        INT NOT NULL,
+
+    PRIMARY KEY(nome),
+    FOREIGN KEY(nome) REFERENCES Participantes(nome)
+);
+
+CREATE TABLE Estudante (
+    nome                VARCHAR NOT NULL,
+    comprovativo        VARCHAR NOT NULL,
+
+    PRIMARY KEY(nome),
+    FOREIGN KEY(nome) REFERENCES Participantes(nome)
 );
