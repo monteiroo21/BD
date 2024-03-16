b241633a0b898ade1a8db8d55cb8b31aa0c0b541
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-03-16 12:57:42 +0000

    4_1 sql

---
+++ aula4/ex_4_1_2.sql
@@ -0,0 +1,101 @@
+CREATE DATABASE FLIGHTSRESERVATIONS;
+GO
+USE FLIGHTSRESERVATIONS;
+GO
+
+CREATE TABLE Airport (
+    code        VARCHAR NOT NULL,           
+    [name]      VARCHAR NOT NULL,       
+    city        VARCHAR NOT NULL,           
+    [state]       VARCHAR,      
+
+    PRIMARY KEY(code)
+);
+
+CREATE TABLE CanLand (
+    code_airport    VARCHAR NOT NULL,
+    airplane_type_name VARCHAR NOT NULL,
+
+    PRIMARY KEY(code_airport, airplane_type_name),
+    FOREIGN KEY(code_airport) REFERENCES Airport(code),
+    FOREIGN KEY(airplane_type_name) REFERENCES AirplaneType([type_name])
+);
+
+CREATE TABLE AirplaneType (
+    [type_name]      VARCHAR NOT NULL,
+    capacity       INT NOT NULL,
+    company   VARCHAR NOT NULL,
+
+    PRIMARY KEY([type_name])
+);
+
+CREATE TABLE Airplane (
+    [id]            INT NOT NULL,
+    [type_name]       VARCHAR NOT NULL,
+    total_seats    INT NOT NULL,
+
+    PRIMARY KEY([id]),
+    FOREIGN KEY(type_name) REFERENCES AirplaneType([type_name])
+);
+
+CREATE TABLE Flight (
+    [number]        INT NOT NULL,
+    airline         VARCHAR NOT NULL,
+    weekdays        VARCHAR NOT NULL,
+
+    PRIMARY KEY([number])
+);
+
+CREATE TABLE Fare (
+    fare_code       INT NOT NULL,
+    amount          INT NOT NULL,
+    restrictions    VARCHAR,
+    flight_number   INT NOT NULL,
+
+    PRIMARY KEY(fare_code),
+    FOREIGN KEY(flight_number) REFERENCES Flight([number])
+);
+
+CREATE TABLE FlightLeg (
+    flight_leg_no   INT NOT NULL,
+    flight_number   INT NOT NULL,
+    scheduled_departure_time DATETIME NOT NULL,
+    scheduled_arrival_time DATETIME NOT NULL,
+    airport_code VARCHAR NOT NULL,
+
+    PRIMARY KEY(flight_leg_no, flight_number),
+    FOREIGN KEY(flight_number) REFERENCES Flight([number]),
+    FOREIGN KEY(airport_code) REFERENCES Airport(code)
+);
+
+CREATE TABLE LegInstance (
+    flight_leg_no       INT NOT NULL,
+    flight_number       INT NOT NULL,
+    leg_instance_data   DATETIME NOT NULL,
+    airplane_id         INT NOT NULL,
+    airport_code        VARCHAR NOT NULL,
+    no_of_seats         INT NOT NULL,
+    dep_time    DATETIME NOT NULL,
+    arr_time    DATETIME NOT NULL,
+
+
+    PRIMARY KEY(flight_leg_no, flight_number, leg_instance_data),
+    FOREIGN KEY(flight_leg_no) REFERENCES FlightLeg(flight_leg_no),
+    FOREIGN KEY(flight_number) REFERENCES Flight([number]),
+    FOREIGN KEY(airplane_id) REFERENCES Airplane([id]),
+    FOREIGN KEY(airport_code) REFERENCES Airport(code)
+);
+
+CREATE TABLE Seat (
+    seat_no         INT NOT NULL,
+    customer_name   VARCHAR,
+    cphone          VARCHAR,
+    leg_instance_data DATETIME NOT NULL,
+    flight_leg_no   INT NOT NULL,
+    flight_number   INT NOT NULL,
+
+    PRIMARY KEY(seat_no, leg_instance_data, flight_leg_no, flight_number),
+    FOREIGN KEY(flight_leg_no) REFERENCES FlightLeg(flight_leg_no),
+    FOREIGN KEY(flight_number) REFERENCES Flight([number]),
+    FOREIGN KEY(leg_instance_data) REFERENCES LegInstance(leg_instance_data)
+);
