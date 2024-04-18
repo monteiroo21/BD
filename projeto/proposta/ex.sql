878a6e2f92f9e4646953df0b99cbf6319387add4
João Monteiro <joaomonteiro29@ua.pt>
2024-04-18 10:16:58 +0100

    projeto

--- projeto/proposta/ex.sql
+++ projeto/proposta/ex.sql
@@ -2,14 +2,6 @@ CREATE DATABASE MusicScore;
 GO
 USE MusicScore;
 GO
-CREATE TABLE Instrumentation (
-    instrument      VARCHAR NOT NULL,
-    quantity        INT,
-    family          VARCHAR,
-    [role]          VARCHAR,
-
-    PRIMARY KEY(instrument),
-);
 
 CREATE TABLE MusicalGenre (
     id              INT NOT NULL,
@@ -23,12 +15,10 @@ CREATE TABLE Music (
     music_id        INT NOT NULL,
     title           VARCHAR NOT NULL,
     [year]          INT,
-    musGenre_id     INT NOT NULL,
-    instrumentation VARCHAR NOT NULL,
+    musGenre_id     INT,
 
     PRIMARY KEY(music_id),
-    FOREIGN KEY(musGenre_id) REFERENCES MusicalGenre(id),
-    FOREIGN KEY(instrumentation) REFERENCES Instrumentation(instrument)
+    FOREIGN KEY(musGenre_id) REFERENCES MusicalGenre(id)
 );
 
 CREATE TABLE Editor (
@@ -43,15 +33,26 @@ CREATE TABLE Editor (
 CREATE TABLE Score (
     register_num        INT NOT NULL,
     [edition]           INT,
-    price               MONEY NOT NULL,
-    [availability]      INT NOT NULL,
+    price               MONEY,
+    [availability]      INT,
     difficultyGrade     INT,
     musicId             INT NOT NULL,
-    editorId            INT NOT NULL,
+    editorId            INT,
 
     PRIMARY KEY(register_num),
     FOREIGN KEY(musicId) REFERENCES Music(music_id),
     FOREIGN KEY(editorId) REFERENCES Editor(identifier)
+);
+
+CREATE TABLE Instrumentation (
+    instrument      VARCHAR NOT NULL,
+    quantity        INT,
+    family          VARCHAR,
+    [role]          VARCHAR,
+    scoreNum        INT NOT NULL,
+
+    PRIMARY KEY(instrument),
+    FOREIGN KEY(scoreNum) REFERENCES Score(register_num)
 );
 
 CREATE TABLE Warehouse (
@@ -71,30 +72,18 @@ CREATE TABLE Customer (
     cellNumber          INT,
     [name]              VARCHAR NOT NULL,
 
-    PRIMARY KEY(numCC)
-);
-
-CREATE TABLE StaffPersonel (
-    numCC               INT NOT NULL,
-    email_address       VARCHAR NOT NULL,
-    employeeId          INT NOT NULL,
-    cellNumber          INT,
-    [name]              VARCHAR NOT NULL,
-
-    PRIMARY KEY(employeeId),
-    UNIQUE(numCC)
+    PRIMARY KEY(numCC),
+    UNIQUE(numBankAccount)
 );
 
 CREATE TABLE [Transaction] (
-    transcation_id          INT NOT NULL,
+    transaction_id          INT NOT NULL,
     [value]                 MONEY NOT NULL,
     [date]                  DATE,
     customer_CC             INT NOT NULL,
-    employee_id             INT NOT NULL,
 
-    PRIMARY KEY(transcation_id),
-    FOREIGN KEY(customer_CC) REFERENCES Customer(numCC),
-    FOREIGN KEY(employee_id) REFERENCES StaffPersonel(employeeId)
+    PRIMARY KEY(transaction_id),
+    FOREIGN KEY(customer_CC) REFERENCES Customer(numCC)
 );
 
 CREATE TABLE Writer (
@@ -104,8 +93,10 @@ CREATE TABLE Writer (
     genre           CHAR,
     birthYear       DATE,
     deathYear       DATE,
+    musGenre_id     INT,
 
-    PRIMARY KEY(id)
+    PRIMARY KEY(id),
+    FOREIGN KEY(musGenre_id) REFERENCES MusicalGenre(id)
 );
 
 CREATE TABLE Arranger (
@@ -116,22 +107,30 @@ CREATE TABLE Arranger (
 );
 
 CREATE TABLE Composer (
-    id              INT NOT NULL,
-    musGenre_id     INT NOT NULL,
+    id            INT NOT NULL,
 
     PRIMARY KEY(id),
-    FOREIGN KEY(id) REFERENCES Writer(id),
-    FOREIGN KEY(musGenre_id) REFERENCES MusicalGenre(id)
+    FOREIGN KEY(id) REFERENCES Writer(id)
 );
 
 CREATE TABLE writes (
     music_id            INT NOT NULL,
-    writer_id           INT NOT NULL,
+    composer_id         INT NOT NULL,
 
-    PRIMARY KEY(music_id, writer_id),
+    PRIMARY KEY(music_id, composer_id),
     FOREIGN KEY(music_id) REFERENCES Music(music_id),
-    FOREIGN KEY(writer_id) REFERENCES Writer(id)
+    FOREIGN KEY(composer_id) REFERENCES Composer(id)
 );
+
+CREATE TABLE arranges (
+    score_register      INT NOT NULL,
+    arranger_id         INT NOT NULL,
+    [type]              VARCHAR,
+
+    PRIMARY KEY(score_register, arranger_id),
+    FOREIGN KEY(score_register) REFERENCES Score(register_num),
+    FOREIGN KEY(arranger_id) REFERENCES Arranger(id)
+)
 
 CREATE TABLE stores (
     warehouse_id        INT NOT NULL,
@@ -158,3 +157,12 @@ CREATE TABLE warehouse_location (
     PRIMARY KEY(warehouse_location, warehouse_id),
     FOREIGN KEY(warehouse_id) REFERENCES Warehouse(id)
 );
+
+CREATE TABLE constitutes (
+    score_register      INT NOT NULL,
+    transaction_id      INT NOT NULL,
+
+    PRIMARY KEY(score_register, transaction_id),
+    FOREIGN KEY(score_register) REFERENCES Score(register_num),
+    FOREIGN KEY(transaction_id) REFERENCES Transaction(transaction_id)
+)
