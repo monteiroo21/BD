29984188bd11da1a46f136d72e7e27e76e7a036c
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-05-19 22:58:46 +0100

    composers name

--- projeto/project_code/code/bd-project/bd_project/music.py
+++ projeto/project_code/code/bd-project/bd_project/music.py
@@ -11,14 +11,28 @@ class Music (NamedTuple):
     title: str
     year: int
     genre_name: str
+    composer_fname: str
+    composer_lname: str
 
+
+# def list_allMusic() -> list[Music]:
+#     with create_connection() as conn:
+#         with conn.cursor() as cursor:
+#             cursor.execute("""SELECT music_id, title, [year], g.[name] AS genre_name 
+#                            FROM Music AS m
+#                            JOIN MusicalGenre AS g ON m.musGenre_id = g.id""")
+#             return [Music(*row) for row in cursor.fetchall()]
 
 def list_allMusic() -> list[Music]:
     with create_connection() as conn:
         with conn.cursor() as cursor:
-            cursor.execute("""SELECT music_id, title, [year], g.[name] AS genre_name 
-                           FROM Music AS m
-                           JOIN MusicalGenre AS g ON m.musGenre_id = g.id""")
+            cursor.execute("""SELECT m.music_id, m.title, m.[year], g.[name] AS genre_name, wr.Fname, wr.Lname
+                FROM Music AS m
+                JOIN MusicalGenre AS g ON m.musGenre_id = g.id
+                JOIN writes AS mw ON m.music_id = mw.music_id
+                JOIN Composer AS c ON mw.composer_id = c.id
+                JOIN Writer AS wr ON c.id = wr.id
+                """)
             return [Music(*row) for row in cursor.fetchall()]
         
 
