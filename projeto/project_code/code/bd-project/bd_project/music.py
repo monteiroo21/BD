04adcded66ebb156246da305d9848455267f5c6b
JorgeGuiDomingues <jorgeguilherme@ua.pt>
2024-05-19 15:34:36 +0100

    genres name

--- projeto/project_code/code/bd-project/bd_project/music.py
+++ projeto/project_code/code/bd-project/bd_project/music.py
@@ -10,18 +10,23 @@ class Music (NamedTuple):
     music_id: int
     title: str
     year: int
-    genre: int
+    genre_name: str
 
 
 def list_allMusic() -> list[Music]:
     with create_connection() as conn:
         with conn.cursor() as cursor:
-            cursor.execute("SELECT music_id, title, [year], musGenre_id FROM Music")
+            cursor.execute("""SELECT music_id, title, [year], g.[name] AS genre_name 
+                           FROM Music AS m
+                           JOIN MusicalGenre AS g ON m.musGenre_id = g.id""")
             return [Music(*row) for row in cursor.fetchall()]
         
 
 def search_music(query: str) -> list[Music]:
     with create_connection() as conn:
         with conn.cursor() as cursor:
-            cursor.execute("SELECT music_id, title, [year], musGenre_id FROM Music WHERE title LIKE ?", ('%' + query + '%',))
+            cursor.execute("""SELECT music_id, title, [year], g.[name] AS genre_name 
+                           FROM Music AS m
+                            JOIN MusicalGenre AS g ON m.musGenre_id = g.id
+                            WHERE m.title LIKE ?""", ('%' + query + '%',))
             return [Music(*row) for row in cursor.fetchall()]
