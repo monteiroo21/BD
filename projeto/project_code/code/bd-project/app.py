d70a2b39e5c08ae9c52dd23f7d3bd6bcb323f5d8
monteiroo21 <joaomonteiro29@ua.pt>
2024-05-20 13:41:22 +0100

    composers

--- projeto/project_code/code/bd-project/app.py
+++ projeto/project_code/code/bd-project/app.py
@@ -3,6 +3,7 @@ from flask import Flask, make_response, render_template, render_template_string,
 from bd_project import music
 from bd_project import composer
 from bd_project.music import Music
+from bd_project.composer import Composer
 
 app = Flask(__name__)
 
@@ -24,6 +25,12 @@ def music_search():
     query = request.args.get('query', '')
     musics = music.search_music(query)
     return render_template("music_list.html", musics=musics)
+
+
+@app.route("/composer-list", methods=["GET"])
+def composer_list():
+    composers = composer.list_Composers()
+    return render_template("composer_list.html", composers=composers)
     
 
 if __name__ == "__main__":
